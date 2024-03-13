8147b2e1094bad2f4c81b1ec7b293f04be8b8e1f
orangatun <raghunadhan@protonmail.com>
2024-03-13 08:32:36 -0700

    Adds scroll and navbar active selection
    Clicking on link in navbar scrolls to element
    Scrolling page changes navbar selection
    Handles initial location hash

--- js/03/navbar.js
+++ js/03/navbar.js
@@ -15,3 +15,37 @@ jobTitle.insertAdjacentElement("afterend", oneLiner);
 let resumeLink = document.getElementById("links").lastElementChild;
 resumeLink.style.display="none";
 
+// Active section
+let links = document.getElementById("links").children;
+const sections = document.querySelectorAll("section");
+
+var currentSection = "";
+if(document.location.hash.length===0) {
+    window.scrollTo(0,1);
+} else {
+    let hashVal = document.location.hash.substring(1);
+    sections.forEach(section => {
+        if(hashVal===section.id) {
+            window.scrollTo(0, section.offsetTop+1);
+        }
+    });
+}
+console.log(currentSection);
+window.addEventListener("scroll", function() {
+    var newSection = links[0].getAttribute("href");
+    sections.forEach(section => {
+        if(this.scrollY>=section.offsetTop) {
+            newSection = '#'+section.id;
+        }
+    });
+    if(newSection===currentSection) return;
+    for(const link of links) {
+        if(link.getAttribute("href")===currentSection) {
+            link.classList.remove("active-section");
+        }
+        if(link.getAttribute("href")===newSection) {
+            link.classList.add("active-section");
+        }
+    }
+    currentSection = newSection;
+});
